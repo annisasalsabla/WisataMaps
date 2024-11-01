2ab575aa6a7476812b844ef3ad8b7372276cb656
Annisa Salsabila <nisasalsabila23050505@gmail.com>
2024-11-01 09:29:07 +0700

    start

---
+++ app/src/main/java/model/ModelWisata.kt
@@ -0,0 +1,10 @@
+package model
+
+data class ModelWisata(
+    var image : Int,
+    var nama : String,
+    var daerah : String,
+    var deskripsi : String,
+    val latitude: Double,
+    val longitude: Double
+)
